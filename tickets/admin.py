6dc16e2cc7963e0793016bcb231bc459fe1c904f
Yamii44 <ramiisamiel@gmail.com>
2023-07-06 20:57:29 +0400

    Test

---
+++ tickets/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from.models import Guest,Movie,Reservation,Post
+admin.site.register(Movie)
+admin.site.register(Guest)
+admin.site.register(Reservation)
+admin.site.register(Post)
